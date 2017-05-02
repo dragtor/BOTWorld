1e4ec32ea301024f1f4a5e67c7f57060ad8298bd
Shubham Shahare <shubhamshahare6@gmail.com>
2017-05-02 13:13:08 +0530

    Camera chases robot done

--- robot.js
+++ robot.js
@@ -13,8 +13,6 @@ $(function () {
     }
   });
 });
-
-
 // validate json 
 // validation terms : initial wall , robot should not overlap 
 // cell should display some number on it
@@ -23,8 +21,7 @@ $(function () {
 // robot will follow as languaage
 // exception : coin not present still picking coin
 // robot collision exception
-
-
+// camera position should be set according to object and there should be one key bind for changing cam on robot
 function readCubeDetails(cube) {
   cube_id = JSON.stringify(cube.id);
   dimention_ab = JSON.stringify(cube.dimention.ab);
@@ -115,190 +112,270 @@ function readCameraDetails(camera) {
     'lookAt_z': lookAt_z
   };
 }
-
-function readGridDetails(grid){
+function readGridDetails(grid) {
   numberOfRows = parseInt(JSON.stringify(grid.numberOfRows));
-  
   numberOfCols = parseInt(JSON.stringify(grid.numberOfCols));
   cellSpacing = parseInt(JSON.stringify(grid.cellSpacing));
   cellWidth = parseInt(JSON.stringify(grid.cellProperties.width));
   cellHeight = parseInt(JSON.stringify(grid.cellProperties.height));
   cellColor = parseInt(grid.cellColor);
   return {
-    "numberOfRows" : numberOfRows,
-    "numberOfCols" : numberOfCols ,
-    "cellSpacing":cellSpacing,
-    "cellWidth" :cellWidth ,
-    "cellHeight" :cellHeight,
-    "cellColor" : cellColor
-  }; 
+    'numberOfRows': numberOfRows,
+    'numberOfCols': numberOfCols,
+    'cellSpacing': cellSpacing,
+    'cellWidth': cellWidth,
+    'cellHeight': cellHeight,
+    'cellColor': cellColor
+  };
 }
-
-
-function readWallDetails(wall){
+function readWallDetails(wall) {
   wallId = parseInt(JSON.stringify(wall.id));
   rowPosition = parseInt(JSON.stringify(wall.placement.row));
   colPosition = parseInt(JSON.stringify(wall.placement.col));
   return {
-    "wallId" : wallId,
-    "rowPosition" : rowPosition ,
-    "colPosition":colPosition
-  }; 
+    'wallId': wallId,
+    'rowPosition': rowPosition,
+    'colPosition': colPosition
+  };
 }
-function readRobotDetails(robot){
+function readRobotDetails(robot) {
   robotId = parseInt(JSON.stringify(robot.id));
   rowPosition = parseInt(JSON.stringify(robot.placement.row));
   colPosition = parseInt(JSON.stringify(robot.placement.col));
   color = parseInt(robot.color);
   return {
-    "robotId" :robotId,
-    "rowPosition" : rowPosition ,
-    "colPosition":colPosition ,
-    "color" : color
-  }; 
+    'robotId': robotId,
+    'rowPosition': rowPosition,
+    'colPosition': colPosition,
+    'color': color
+  };
 }
-
 function readPlaneDetails(plane) {
   width = parseInt(JSON.stringify(plane.width));
   height = parseInt(JSON.stringify(plane.height));
   placement_x = parseInt(JSON.stringify(plane.placement.x));
   placement_y = parseInt(JSON.stringify(plane.placement.y));
   placement_z = parseInt(JSON.stringify(plane.placement.z));
-  
   color = parseInt(plane.color);
   alert(color);
   rotation = JSON.stringify(plane.rotation);
-  
-    if (rotation.charAt(0) == '"') {
-      rotation = rotation.slice(1, - 1);
-    }
-    if (rotation == 'default') {
-      rotation_x = -0.5 * Math.PI ;
-      rotation_y = 0;
-      rotation_z = 0;
-    } else {
-      rotation_x = parseInt(JSON.stringify(plane.rotation.x));
-      rotation_y = parseInt(JSON.stringify(plane.rotation.y));
-      rotation_z = parseInt(JSON.stringify(plane.rotation.z));
-    }
-  
+  if (rotation.charAt(0) == '"') {
+    rotation = rotation.slice(1, - 1);
+  }
+  if (rotation == 'default') {
+    rotation_x = - 0.5 * Math.PI;
+    rotation_y = 0;
+    rotation_z = 0;
+  } else {
+    rotation_x = parseInt(JSON.stringify(plane.rotation.x));
+    rotation_y = parseInt(JSON.stringify(plane.rotation.y));
+    rotation_z = parseInt(JSON.stringify(plane.rotation.z));
+  }
   return {
-    "width" : width,
-    "height" : height ,
-    "placement_x":placement_x,
-    "placement_y" :placement_y ,
-    "placement_z" :placement_z ,
-    "color" : color ,
-    "rotation_x" : rotation_x,
-    "rotation_y" : rotation_y ,
-    "rotation_z" : rotation_z
+    'width': width,
+    'height': height,
+    'placement_x': placement_x,
+    'placement_y': placement_y,
+    'placement_z': placement_z,
+    'color': color,
+    'rotation_x': rotation_x,
+    'rotation_y': rotation_y,
+    'rotation_z': rotation_z
   };
 }
 function init(myObj) {
-  
   var scene = new THREE.Scene();
   var clock = new THREE.Clock();
-
   var cameraDetails = readCameraDetails(myObj.objects.camera);
-  if (cameraDetails.type == 'perspective'){
-   var camera = new THREE.PerspectiveCamera(cameraDetails.field_of_view, cameraDetails.aspect_ratio, cameraDetails.near, cameraDetails.far);
+  if (cameraDetails.type == 'perspective') {
+    var camera = new THREE.PerspectiveCamera(cameraDetails.field_of_view, cameraDetails.aspect_ratio, cameraDetails.near, cameraDetails.far);
   }
   camera.position.x = cameraDetails.placement_x;
   camera.position.y = cameraDetails.placement_y;
-  camera.position.z = cameraDetails.placement_z; 
- 
- 
+  camera.position.z = cameraDetails.placement_z;
   var flyControls = new THREE.FlyControls(camera);
-       flyControls.movementSpeed = 25;
-       flyControls.domElement = document.querySelector('#WebGL-output');
-       flyControls.rollSpeed = Math.PI / 24;
-       flyControls.autoForward = false;
-       flyControls.dragToLook = false;
- 
-
+  flyControls.movementSpeed = 25;
+  flyControls.domElement = document.querySelector('#WebGL-output');
+  flyControls.rollSpeed = Math.PI / 24;
+  flyControls.autoForward = false;
+  flyControls.dragToLook = false;
+  flyControls.enabled = false;
   var gridDetails = readGridDetails(myObj.objects.grid);
- 
   var cellX = gridDetails.cellWidth; // by user
   var cellY = 4; // do not change this
   var cellZ = gridDetails.cellHeight; // by user
-
- 
-  var numberOfRows = gridDetails.numberOfRows;//by user
+  var numberOfRows = gridDetails.numberOfRows; //by user
   var numberOfCols = gridDetails.numberOfCols; // by user
   var spacing = gridDetails.cellSpacing; //default or by user
-  
   var planeDetails = readPlaneDetails(myObj.objects.plane);
-  var planeWidth = (cellX * numberOfCols) + spacing *(numberOfCols +1);
-  var planeHeight = (cellZ * numberOfRows) + spacing * (numberOfRows +1 );
-  var planeGeometry = new THREE.PlaneGeometry(planeWidth,planeHeight);
-  var planeMaterial = new THREE.MeshLambertMaterial({color: planeDetails.color});
+  var planeWidth = (cellX * numberOfCols) + spacing * (numberOfCols + 1);
+  var planeHeight = (cellZ * numberOfRows) + spacing * (numberOfRows + 1);
+  var planeGeometry = new THREE.PlaneGeometry(planeWidth, planeHeight);
+  var planeMaterial = new THREE.MeshLambertMaterial({
+    color: planeDetails.color
+  });
   var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-  
   plane.rotation.x = planeDetails.rotation_x;
   plane.position.x = planeDetails.placement_x;
   plane.position.y = planeDetails.placement_y;
   plane.position.z = planeDetails.placement_z;
-  scene.add( plane );
-  
-  var cellGeometry = new THREE.BoxGeometry(cellX,cellY,cellZ);
-  var cellMaterial = new THREE.MeshLambertMaterial({color: gridDetails.cellColor});
-   for (var j = 0; j < numberOfCols; j++) {
-     for (var i = 0; i < numberOfRows; i++) {
-                var cell = new THREE.Mesh(cellGeometry, cellMaterial);
-                cell.position.z = -((planeGeometry.parameters.height) / 2) +cellZ/2 + spacing +(spacing +cellZ)*i;
-                cell.position.x = -((planeGeometry.parameters.width) / 2) +cellX/2 + spacing +(spacing +cellX)*j;
-                cell.position.y = 2; //do not change this
-                scene.add(cell);
-            }
-        }
-
-   numberOfWalls = (myObj.objects.walls).length;
-   for ( var i = 0 ; i < numberOfWalls ; i++ ){
+  scene.add(plane);
+  var cellGeometry = new THREE.BoxGeometry(cellX, cellY, cellZ);
+  var cellMaterial = new THREE.MeshLambertMaterial({
+    color: gridDetails.cellColor
+  });
+  for (var j = 0; j < numberOfCols; j++) {
+    for (var i = 0; i < numberOfRows; i++) {
+      var cell = new THREE.Mesh(cellGeometry, cellMaterial);
+      cell.position.z = - ((planeGeometry.parameters.height) / 2) + cellZ / 2 + spacing + (spacing + cellZ) * i;
+      cell.position.x = - ((planeGeometry.parameters.width) / 2) + cellX / 2 + spacing + (spacing + cellX) * j;
+      cell.position.y = 2; //do not change this
+      scene.add(cell);
+    }
+  }
+  numberOfWalls = (myObj.objects.walls).length;
+  for (var i = 0; i < numberOfWalls; i++) {
     var wallJson = myObj.objects.walls[i];
     var wallDetails = readWallDetails(wallJson);
-    var wallGeometry = new THREE.BoxGeometry(cellX,80,cellZ);
-    var wallMaterial = new THREE.MeshLambertMaterial({color: 0xffee22});
+    var wallGeometry = new THREE.BoxGeometry(cellX, 80, cellZ);
+    var wallMaterial = new THREE.MeshLambertMaterial({
+      color: 16772642
+    });
     var wall = new THREE.Mesh(wallGeometry, wallMaterial);
-
-                wall.position.z = -((planeGeometry.parameters.height) / 2) +cellZ/2 + spacing +(spacing +cellZ)*(wallDetails.rowPosition - 1);
-                wall.position.x = -((planeGeometry.parameters.width) / 2) +cellX/2 + spacing +(spacing +cellX)*(wallDetails.colPosition - 1);
-                wall.position.y = 43; //do not change this
-                scene.add(wall);  
-   }
-  
+    wall.position.z = - ((planeGeometry.parameters.height) / 2) + cellZ / 2 + spacing + (spacing + cellZ) * (wallDetails.rowPosition - 1);
+    wall.position.x = - ((planeGeometry.parameters.width) / 2) + cellX / 2 + spacing + (spacing + cellX) * (wallDetails.colPosition - 1);
+    wall.position.y = 43; //do not change this
+    scene.add(wall);
+  }
   numberOfRobots = (myObj.objects.robots).length;
-  for ( var i = 0 ; i < numberOfRobots ; i++){
+  var robotPosition = {};
+  for (var i = 0; i < numberOfRobots; i++) {
     var robotJson = myObj.objects.robots[i];
     var robotDetails = readRobotDetails(robotJson);
-    var robotGeometry = new THREE.BoxGeometry(cellX,25,cellZ);
-    var robotMaterial = new THREE.MeshLambertMaterial({color: robotDetails.color});
-    var robot = new THREE.Mesh(robotGeometry, robotMaterial);
+    var robotGeometry = new THREE.BoxGeometry(cellX, 25, cellZ);
+    var robotMaterial = new THREE.MeshLambertMaterial({
+      color: robotDetails.color
+    });
     
-     robot.position.z = -((planeGeometry.parameters.height) / 2) +cellZ/2 + spacing +(spacing +cellZ)*(robotDetails.rowPosition - 1);
-     robot.position.x = -((planeGeometry.parameters.width) / 2) +cellX/2 + spacing +(spacing +cellX)*(robotDetails.colPosition - 1);
-     robot.position.y = 17; //do not change this
-     scene.add(robot); 
-  }
+    var robot = new THREE.Mesh(robotGeometry, robotMaterial);
+    robot.position.z = - ((planeGeometry.parameters.height) / 2) + cellZ / 2 + spacing + (spacing + cellZ) * (robotDetails.rowPosition - 1);
+    robot.position.x = - ((planeGeometry.parameters.width) / 2) + cellX / 2 + spacing + (spacing + cellX) * (robotDetails.colPosition - 1);
+    robot.position.y = 17; //do not change this
+    robot.name = 'robot' + (i + 1).toString();
+    var randomnumber = Math.floor(Math.random() * (3- 0 + 1)) + 0;
+    robotPosition[robot.name] = {'row':robotDetails.rowPosition , 'col' :robotDetails.colPosition , direction : randomnumber };
+     var rotateAngle = Math.PI / 2;
   
-  var ambientLight = new THREE.AmbientLight(0x292929);
+    for (var k = 0 ; k< randomnumber ; k++){
+      robot.rotateOnAxis(new THREE.Vector3(0, 1, 0), -rotateAngle);
+    } 
+    scene.add(robot);
+  }
+  console.log(robotPosition['robot1'].row);
+  var ambientLight = new THREE.AmbientLight(2697513);
   scene.add(ambientLight);
-  var directionalLight = new THREE.DirectionalLight(0xffffff, 0.7);
-  directionalLight.position.set(-20, 40, 60);
+  var directionalLight = new THREE.DirectionalLight(16777215, 0.7);
+  directionalLight.position.set( - 20, 40, 60);
   scene.add(directionalLight);
-
- 
-
+  var keyboard = new THREEx.KeyboardState();
   var renderer = new THREE.WebGLRenderer();
   renderer.setClearColor(new THREE.Color(15658734, 1));
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.shadowMapEnabled = true;
-
   document.getElementById('WebGL-output').appendChild(renderer.domElement);
   render();
+  
+ // alert(robotPosition['robot1']);
   function render() {
     var delta = clock.getDelta();
-    flyControls.update(delta);
+    var rotateAngle = Math.PI / 2;
+    //var moveDistance = 200; // 200 pixels per second
+   // flyControls.update(delta);
+    r1 = scene.getObjectByName('robot1');
+     
+    if (keyboard.pressed('W')) {
+        // anticlockwise  //turn left by 90 Degree
+      // alert(robotPosition['robot1'].direction);
+        r1.rotateOnAxis(new THREE.Vector3(0, 1, 0), rotateAngle);// To rotate robot r1 by angle
+        var robotDir = robotPosition['robot1'].direction; 
+      robotPosition['robot1'].direction = ((robotDir - 1)== -1) ? 3 : (robotDir-1);
+
+    }
+     
+    if (keyboard.pressed('E')) {
+      // clockwise  //turn left by 90 Degree
+     // alert(robotPosition['robot1'].direction);
+        r1.rotateOnAxis(new THREE.Vector3(0, 1, 0), -rotateAngle);// To rotate robot r1 by angle
+        var robotDir = robotPosition['robot1'].direction; 
+        robotPosition['robot1'].direction = ((robotDir + 1)== 4) ? 0 : (robotDir+1);
+
+    }
+    if (keyboard.pressed('S')){
+     
+       robotRowPosition = robotPosition['robot1'].row;
+       robotColPosition = robotPosition['robot1'].col;
+       robotDir = robotPosition['robot1'].direction;
+      alert(robotDir);
+      if(robotDir == 0){
+       r1.position.z = - ((planeGeometry.parameters.width) / 2) + cellZ/ 2 + spacing + (spacing + cellZ) * ( (robotRowPosition -1) - 1);
+       robotPosition['robot1'] = {'row': (robotRowPosition-1)  , 'col' : robotColPosition , direction : robotDir };
+      
+     }else if(robotDir == 1){
+       r1.position.x = - ((planeGeometry.parameters.height) / 2) + cellX / 2 + spacing + (spacing + cellX) * ((robotRowPosition+1) - 1);
+       robotPosition['robot1'] = {'row': (robotRowPosition+1)  , 'col' :(robotColPosition) , direction : robotDir };
+     
+     }else if(robotDir == 2){
+       //r1.position.x = - ((planeGeometry.parameters.width) / 2) + cellX / 2 + spacing + (spacing + cellX) * ( (robotRowPosition+1) - 1);
+       r1.position.z = - ((planeGeometry.parameters.width) / 2) + cellZ/ 2 + spacing + (spacing + cellZ) * ( (robotRowPosition +1) - 1);
+       robotPosition['robot1'] = {'row': (robotRowPosition+1)  , 'col' : robotColPosition , direction : robotDir };
+       //r1.position.z = - ((planeGeometry.parameters.width) / 2) + cellZ / 2 + spacing + (spacing + cellZ) * ( (robotRowPosition+1) - 1); //right
+       //robotPosition['robot1'] = {'row': (robotRowPosition+1)  , 'col' :robotColPosition , direction : robotDir };
+     
+     }else if(robotDir == 3){
+       r1.position.x = - ((planeGeometry.parameters.height) / 2) + cellX / 2 + spacing + (spacing + cellX) * ((robotRowPosition-1) - 1);
+       robotPosition['robot1'] = {'row': (robotRowPosition-1)  , 'col' :(robotColPosition) , direction : robotDir };
+     }
+    }
+   
+    if (keyboard.pressed('Z')) {
+       alert("cam changes");
+       //set initial camera
+   /*   var relativeCameraOffset = new THREE.Vector3(0, 20, 120);
+    var cameraOffset = relativeCameraOffset.applyMatrix4(r1.matrixWorld);
+    camera.position.x = cameraOffset.x;
+    camera.position.y = cameraOffset.y;
+    camera.position.z = cameraOffset.z;*/
+    
+        var robotDir = robotPosition['robot1'].direction; 
+        if(robotDir == 0){
+          alert("yes called0");
+          r1.rotation.y =  Math.PI / 2;
+          camera.rotation.x = 0;
+          
+        }else if(robotDir == 1){
+          alert("yes called1");
+          camera.rotation.x = 90 * Math.PI / 180 ;
+         /* */
+        }else if(robotDir == 2){
+          r1.rotation.y =  Math.PI / 2;
+          alert("yes called2");
+          camera.rotation.y = 90*2 * Math.PI / 180 ;
+        }else if(robotDir == 3){
+          alert("yes called3");
+          camera.rotation.x = 90*3 * Math.PI / 180 ;
+        }
+      
+     }
+    
+    var relativeCameraOffset = new THREE.Vector3(0, 20, 120);//0,20,120
+    var cameraOffset = relativeCameraOffset.applyMatrix4(r1.matrixWorld);
+    camera.position.x = cameraOffset.x;
+    camera.position.y = cameraOffset.y;
+    camera.position.z = cameraOffset.z;
+    camera.lookAt(r1.position);
+    
     requestAnimationFrame(render);
     renderer.render(scene, camera);
   }
+ 
 }
